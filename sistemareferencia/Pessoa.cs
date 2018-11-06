270dbc50b3aafe62212d697027cc51992163271b
Ramon Costa <ramon.rodrigues.lima@gmail.com>
2018-11-05 23:31:51 -0400

    Commit PJt Referencia com Objeto

---
+++ sistemareferencia/Pessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace sistemareferencia
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public string Profissao { get; set; }
+
+        public Pessoa(string nome, string profissao)
+        {
+            Nome = nome;
+            Profissao = profissao;
+        }
+
+        public void DizerOi()
+        {
+            Console.WriteLine($"Oi! Meu nome é {Nome} e eu sou {Profissao}");
+        }
+    }
+}
---
+++ sistemareferencia/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace sistemareferencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa pessoa = new Pessoa("Ramon", "Developer");
+            pessoa.DizerOi();
+
+            Pessoa pessoa2 = pessoa;
+
+            pessoa2.Nome = "Maria";
+            pessoa2.Profissao = "Atriz";
+
+            pessoa.DizerOi();
+            pessoa2.DizerOi();
+
+            Console.ReadKey();
+        }
+    }
+}
